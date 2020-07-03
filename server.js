b5fb54873b7d74aef5aae9cb16198b24df9417bc
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-07-03 08:44:45 -0700

    .

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require("path");
 app.use("/api/v1/cases", require("./api/v1/searchedOrderedCases"));
 app.use("/api/v1/allCases", require("./api/v1/allCases"));
 
-//app.use(express.static("client/build"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 3012;
 app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+   console.log(`Server running at http://localhost:${port}`)
 );
